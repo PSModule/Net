95d575379727fb6af1e3767e678d80ed33e68ba6
Marius Storhaug <marstor@hotmail.com>
2025-08-27 11:58:17 +0200

    Remove deprecated files and classes from the module

--- tests/PSModuleTest.Tests.ps1
+++ tests/PSModuleTest.Tests.ps1
@@ -1,24 +1,25 @@
-﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
-    'PSReviewUnusedParameter', '',
-    Justification = 'Required for Pester tests'
-)]
+﻿#Requires -Modules @{ ModuleName = 'Pester'; RequiredVersion = '5.7.1' }
+
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
     'PSUseDeclaredVarsMoreThanAssignments', '',
-    Justification = 'Required for Pester tests'
+    Justification = 'Pester grouping syntax: known issue.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingConvertToSecureStringWithPlainText', '',
+    Justification = 'Used to create a secure string for testing.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingWriteHost', '',
+    Justification = 'Log outputs to GitHub Actions logs.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidLongLines', '',
+    Justification = 'Long test descriptions and skip switches'
 )]
 [CmdletBinding()]
 param()
 
 Describe 'Module' {
-    It 'Function: Get-PSModuleTest' {
-        Get-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
-    }
-    It 'Function: New-PSModuleTest' {
-        New-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
-    }
-    It 'Function: Set-PSModuleTest' {
-        Set-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
-    }
     It 'Function: Test-PSModuleTest' {
         Test-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
     }
